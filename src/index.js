379ef9f95b487cf61d49cc7eba42d5bf2124c00b
Stefan Quest <stefan.quest@gmail.com>
2019-07-17 00:41:17 +0200

    this is using hooks and extracting all the logic to another file in src

--- src/index.js
+++ src/index.js
@@ -1,71 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from "./SeasonDisplay";
 import Loader from "./Loader";
+import useLocation from './useLocation'
 
-// super must be called to pull props from React.Component
-// we use null when we know its an unknown number!
-// changing state will trigger a re-render of the component
-// only setState can be used to change state
+const App =()=> {
 
+const [lat,errorMessage] = useLocation();
 
-
-class App extends React.Component{
-    //****************initializing state using constructor*************** */
-    // constructor(props){
-    //     super(props);
-    //     // THIS IS THE ONLY TIME DIRECT ASSIGNMENT  OF this.state !!!
-    //     this.state = { lat: null, errorMessage: '' };   
-    // }
-
-//***************** */ ALTERNATE STATE INITIALIZATION **********************
-
- state = {lat: null, errorMessage : ''};
-
-
-        componentDidMount(){
-            // the work or get function is NEVER in the render method to avoid double or perpetually calling
-            // this async stuff below can also go into the constructor but it is not advised.
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                //we called setState - this takes the new and replaces the old triggering the rerender
-                this.setState ({lat: position.coords.latitude });
-
-                // IMPORTANT !!! notice how we did NOT do 
-                // this.state.lat = position.coords.latitude
-            },
-            err => {
-                this.setState ({ errorMessage: err.message});
-            }
-          );
-        }
-renderContent(){
-    if ( this.state.errorMessage && !this.state.lat){
-        return (<div>Error: {this.state.errorMessage}</div>);
-    }
-    else if (this.state.lat && !this.state.errorMessage){
-        return <SeasonDisplay  lat= {this.state.lat} />;
-    }
-    else {
-        return <Loader message = "Please accept location request"/>;
-    }
+let content ; 
+if (errorMessage) {
+    content = <div>Error :{errorMessage}</div>;
+} else if (lat){
+    content = <SeasonDisplay lat ={lat} />
+}
+else {
+    content = <Loader message="Please accept location request" />
 }
 
+return <div className="border red">{content}</div>;
+};
 
-
-
-    //React says you MUST define render
-    render(){
-      return ( 
-            <div className="border red">
-            {this.renderContent()}
-            </div>
-      ) 
-    };
-
-        
-    }
-   
 
 
 ReactDOM.render(<App />, document.querySelector("#root"));
